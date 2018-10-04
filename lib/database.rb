990d98af7359325da0277056448d4a16f8ad97da
Muhammad Saiful Islam <saiful.islam@bukalapak.com>
2018-10-04 15:49:04 +0700

    Put config in environment variables

--- lib/database.rb
+++ lib/database.rb
@@ -1,8 +1,9 @@
 require 'sequel'
 require 'yaml'
+require 'erb'
 
 db_config = File.join(File.dirname(__FILE__), '../db/config.yml')
-Sequel.connect(YAML.load(File.read(db_config)))
+Sequel.connect(YAML.load(ERB.new(File.read(db_config)).result))
 
 require 'database/todo'
 require 'database/todoservice'
